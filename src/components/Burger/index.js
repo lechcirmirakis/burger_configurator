df6d01b49a176f30723cb285f4a8f034039487ba
lechcirmirakis <lech.cirmirakis@go2.pl>
2020-09-02 18:03:33 +0200

    Outputting Burger Items dynamically

--- src/components/Burger/index.js
+++ src/components/Burger/index.js
@@ -2,12 +2,17 @@ import React from "react";
 import { BurgerWrapper } from "./styles";
 import BurgerItem from "./BurgerItem/";
 
-const burger = () => {
+const burger = ({ items }) => {
+  const transformedItems = Object.keys(items).map(itKey => {
+    return [...Array(items[itKey])].map((_, i) => (
+      <BurgerItem key={itKey + i} type={itKey} />
+    ));
+  });
+
   return (
     <BurgerWrapper>
       <BurgerItem type="bread-top"></BurgerItem>
-      <BurgerItem type="meat"></BurgerItem>
-      <BurgerItem type="cheese"></BurgerItem>
+      {transformedItems}
       <BurgerItem type="bread-bottom"></BurgerItem>
     </BurgerWrapper>
   );
--- src/containers/BurgerBuilder/index.js
+++ src/containers/BurgerBuilder/index.js
@@ -2,10 +2,19 @@ import React, { Component } from "react";
 import Burger from "../../components/Burger";
 
 class BurgerBuilder extends Component {
+  state = {
+    items: {
+      salad: 1,
+      bacon: 2,
+      cheese: 1,
+      meat: 2,
+    },
+  };
+
   render() {
     return (
       <>
-        <Burger />
+        <Burger items={this.state.items} />
         <div>Build Controls</div>
       </>
     );
