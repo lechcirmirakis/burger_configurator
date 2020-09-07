2617da12aa3573a71681247b19ae38eb007e5117
lechcirmirakis <lech.cirmirakis@go2.pl>
2020-09-07 11:58:05 +0200

    little performance improve

--- src/components/UI/Modal/index.js
+++ src/components/UI/Modal/index.js
@@ -1,21 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
 import { ModalWrapper } from "./styles";
 import Backdrop from "../Backdrop/";
 import PropTypes from "prop-types";
 
-const modal = ({ show, children, cancelOrderHandler }) => {
-  return (
-    <>
-      <Backdrop show={show} click={cancelOrderHandler} />
-      <ModalWrapper show={show}>{children}</ModalWrapper>
-    </>
-  );
-};
+class Modal extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.show !== this.props.show;
+  }
 
-modal.propTypes = {
+  render() {
+    const { show, children, cancelOrderHandler } = this.props;
+
+    return (
+      <>
+        <Backdrop show={show} click={cancelOrderHandler} />
+        <ModalWrapper show={show}>{children}</ModalWrapper>
+      </>
+    );
+  }
+}
+
+Modal.propTypes = {
   show: PropTypes.bool.isRequired,
   children: PropTypes.node.isRequired,
   cancelOrderHandler: PropTypes.func.isRequired,
 };
 
-export default modal;
+export default Modal;
